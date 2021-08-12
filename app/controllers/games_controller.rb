0972d2f11b80a8e6b3c5c7ce495f49436024914d
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-11 21:35:38 -0700

    Completed the validation of the word and the score page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @letters = []
@@ -5,5 +8,21 @@ class GamesController < ApplicationController
   end
 
   def score
+    @word = params[:word].upcase
+    @letter_list = params[:letters]
+    # check if in letters
+    array = @word.split("")
+    match = array.all? {|e| @letter_list.split("").include?(e)}
+    # check if in dictionnary
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    data_serialized = URI.open(url).read
+    attempt = JSON.parse(data_serialized)
+    valid = attempt["found"]
+    # send message
+    if valid
+      @message = "valid"
+    elsif match == false
+      @message = "no_match"
+    end
   end
 end
